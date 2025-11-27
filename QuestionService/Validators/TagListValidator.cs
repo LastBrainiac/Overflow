ffd9451c6890825a3c6d407647a851701279c901
AttilaLudanszki <attila.ludanszki@pactic.com>
2025-11-27 20:28:18 +0100

    End of section 3

---
+++ QuestionService/Validators/TagListValidator.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuestionService.Validators
+{
+    public class TagListValidator(int min, int max) : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is List<string> tags)
+            {
+                if (tags.Count >= min && tags.Count <= max)
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult($"The number of tags must be between {min} and {max}. Current count: {tags.Count}");
+                }
+            }
+            return new ValidationResult("Invalid tag list");
+        }
+    }
+}
